a42758a028560d69c7b4416b702ddf7a854843c0
Juanwu <juanwu@purdue.edu>
2025-11-29 21:32:03 -0600

    feat: Updated grid visualization function to use jax array
    Signed-off-by: Juanwu <juanwu@purdue.edu>

--- src/utilities/visualization.py
+++ src/utilities/visualization.py
@@ -1,45 +1,51 @@
 import typing
 
-import numpy as np
-import numpy.typing as npt
+import jax
+from jax import numpy as jnp
+
+from src.utilities import logging
 
 
 def make_grid(
-    images: typing.Union[typing.Any, npt.NDArray],
+    images: jax.Array,
     n_rows: int = 8,
+    n_cols: int = 8,
     padding: int = 2,
-) -> npt.NDArray:
+) -> jax.Array:
     r"""Convert a batch of images into a grid for visualization.
 
     Args:
-        images (Any | NDArray): Batch of images with shape `(B, H, W, C)`.
-        n_rows (int, optional): Number of rows in grid. Default is :math:`8`.
+        images (jax.Array): Batch of images with shape `(B, H, W, C)`.
+        n_rows (int): Number of rows in grid. Default is :math:`8`.
+        n_cols (int): Number of columns in grid. Default is :math:`8`.
         padding (int, optional): Number of pixels between pair of images.
             Default is :math:`2`.
 
     Returns:
         The array containing a grid of input images.
     """
-    images = np.asarray(images)
-    if images.ndim != 4:
-        raise ValueError(
-            "Input images must be a 4-D numpy array with shape (B, H, W, C). "
-            f"But got {images}."
-        )
-    bz, h, w, c = images.shape
-    n_cols = int(np.ceil(bz / n_rows))
+    images = jnp.reshape(images, (-1,) + images.shape[-3:])
+    _, h, w, c = images.shape
     shape = (
         h * n_rows + padding * (n_rows - 1),
         w * n_cols + padding * (n_cols - 1),
         c,
     )
-    out = np.zeros(shape, dtype=images.dtype)
+    out = jnp.zeros(shape, dtype=images.dtype)
 
     for idx, img in enumerate(images):
         row = idx // n_cols
         col = idx % n_cols
         top = row * (h + padding)
         left = col * (w + padding)
-        out[top : top + h, left : left + w] = img
+        out = out.at[top : top + h, left : left + w].set(img)
+
+        if idx + 1 >= n_rows * n_cols:
+            logging.rank_zero_warning(
+                "Number of images exceed grid capacity; "
+                + "only the first %d images are used.",
+                n_rows * n_cols,
+            )
+            break
 
     return out
