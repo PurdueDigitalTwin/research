a4d04e7047d830ef9c284ac16afdd1148301027f
Juanwu <juanwu@purdue.edu>
2025-12-04 13:37:35 -0800

    feat: Implements the inception b block in inception architecture
    Signed-off-by: Juanwu <juanwu@purdue.edu>

--- src/projects/generative/tools/fid/inception.py
+++ src/projects/generative/tools/fid/inception.py
@@ -81,7 +81,7 @@ class ConvBNReLU(nn.Module):
 
 
 class InceptionABlock(nn.Module):
-    r"""An Inception block as defined in Figure 5 of the Inception v3 paper.
+    r"""An Inception block comprises 5x5, 3x3, 1x1 convolutions and avg pooling.
 
     Args:
         pooled_features (int): Number of output features for the pooling branch.
@@ -204,5 +204,97 @@ class InceptionABlock(nn.Module):
 
         return jnp.concatenate(
             [out_1x1, out_5x5, out_3x3, out_pool],
+            axis=-1,
+        )
+
+
+class InceptionBBlock(nn.Module):
+    r"""An Inception block comprises 3x3, 1x1 convolution and max pooling.
+
+    Args:
+        deterministic (bool, optional): Whether to apply running averages
+            in batch normalization.
+        dtype (Any): The dtype of the computation.
+        param_dtype (Any): The dtype of the parameters.
+    """
+
+    deterministic: typing.Optional[bool] = None
+    dtype: typing.Any = None
+    param_dtype: typing.Any = None
+
+    @nn.compact
+    def __call__(
+        self, inputs: jax.Array, deterministic: typing.Optional[bool] = None
+    ) -> jax.Array:
+        r"""Forward pass of the Inception-B block.
+
+        Args:
+            inputs (jax.Array): Input array of shape `(*, height, width, C)`.
+            deterministic (bool, optional): Whether to apply running averages
+                in batch normalization.
+
+        Returns:
+            Output array of shape `(*, new_height, new_width, features)`.
+        """
+        m_deterministic = nn.merge_param(
+            "deterministic",
+            self.deterministic,
+            deterministic,
+        )
+
+        # branch 1: 3x3 convolution with stride 2
+        branch_3x3 = ConvBNReLU(
+            features=384,
+            kernel_size=3,
+            strides=2,
+            padding="VALID",
+            dtype=self.dtype,
+            param_dtype=self.param_dtype,
+            name="branch_3x3",
+        )
+        out_3x3 = branch_3x3(inputs, deterministic=m_deterministic)
+
+        # branch 2: a cascade of three convolutions
+        branch_3x3dbl_1 = ConvBNReLU(
+            features=64,
+            kernel_size=1,
+            strides=1,
+            padding=0,
+            dtype=self.dtype,
+            param_dtype=self.param_dtype,
+            name="branch_3x3dbl_1",
+        )
+        branch_3x3dbl_2 = ConvBNReLU(
+            features=96,
+            kernel_size=3,
+            strides=1,
+            padding=1,
+            dtype=self.dtype,
+            param_dtype=self.param_dtype,
+            name="branch_3x3dbl_2",
+        )
+        branch_3x3dbl_3 = ConvBNReLU(
+            features=96,
+            kernel_size=3,
+            strides=2,
+            padding=0,
+            dtype=self.dtype,
+            param_dtype=self.param_dtype,
+            name="branch_3x3dbl_3",
+        )
+        out_3x3dbl = branch_3x3dbl_1(inputs, deterministic=m_deterministic)
+        out_3x3dbl = branch_3x3dbl_2(out_3x3dbl, deterministic=m_deterministic)
+        out_3x3dbl = branch_3x3dbl_3(out_3x3dbl, deterministic=m_deterministic)
+
+        # branch 3: max pooling
+        out_pool = nn.max_pool(
+            inputs,
+            window_shape=(3, 3),
+            strides=(2, 2),
+            padding="VALID",
+        )
+
+        return jnp.concatenate(
+            [out_3x3, out_3x3dbl, out_pool],
             axis=-1,
         )
--- src/projects/generative/tools/fid/test_inception.py
+++ src/projects/generative/tools/fid/test_inception.py
@@ -52,5 +52,24 @@ def test_inception_a_block(dtype: typing.Any) -> None:
     assert output.dtype == dtype
 
 
+@pytest.mark.parametrize("dtype", [jnp.float32, jnp.bfloat16])
+def test_inception_b_block(dtype: typing.Any) -> None:
+    r"""Test the `InceptionBBlock` module."""
+    module = inception.InceptionBBlock(
+        dtype=dtype,
+        param_dtype=dtype,
+    )
+    test_input = jnp.ones((1, 35, 35, 288), dtype=dtype)
+    variables = module.init(
+        jax.random.PRNGKey(0),
+        test_input,
+        deterministic=True,
+    )
+    output = module.apply(variables, test_input, deterministic=True)
+    assert isinstance(output, jax.Array)
+    assert output.shape == (1, 17, 17, 768)
+    assert output.dtype == dtype
+
+
 if __name__ == "__main__":
     sys.exit(pytest.main(["-xv", __file__]))
