7f76de7dd4378a491b15ca3f54bd64f291423263
Juanwu <juanwu@purdue.edu>
2025-12-30 09:56:11 -0500

    feat: Implements the building blocks for Diffusion Transformer
    Signed-off-by: Juanwu <juanwu@purdue.edu>

--- src/projects/generative/model/dit.py
+++ src/projects/generative/model/dit.py
@@ -26,6 +26,34 @@ def modulate(
     return inputs * (1 + scale[..., None, :]) + shift[..., None, :]
 
 
+@jax.jit
+def quick_gelu(x: jax.Array) -> jax.Array:
+    r"""Applies the Quick GELU activation function.
+
+    Args:
+        x (jax.Array): Input tensor.
+
+    Returns:
+        Activated tensor.
+    """
+    return x * jax.nn.sigmoid(1.702 * x)
+
+
+@jax.jit
+def approx_gelu_tanh(x: jax.Array) -> jax.Array:
+    r"""Applies the approximate GELU activation function using Tanh.
+
+    Args:
+        x (jax.Array): Input tensor.
+
+    Returns:
+        Activated tensor.
+    """
+    sqrt_2_over_pi = jnp.sqrt(2 / jnp.pi)
+    x_cubed = jnp.power(x, 3)
+    return 0.5 * x * (1 + jnp.tanh(sqrt_2_over_pi * (x + 0.044715 * x_cubed)))
+
+
 # ==============================================================================
 # Modules
 class SelfAttention(nn.Module):
@@ -116,7 +144,7 @@ class SelfAttention(nn.Module):
                 name="k_norm",
             )(k)
 
-        q_scaled = q / jnp.sqrt(jnp.sqrt(q.shape[-1]).astype(self.dtype))
+        q_scaled = q / jnp.sqrt(jnp.sqrt(k.shape[-1]).astype(self.dtype))
         k_scaled = k / jnp.sqrt(jnp.sqrt(k.shape[-1]).astype(self.dtype))
         attn_weights = jnp.einsum(
             "...qhd,...khd ->...hqk",
@@ -172,28 +200,262 @@ class SelfAttention(nn.Module):
         return out
 
 
-class _DiTConditioningBlock(nn.Module):
+class FeedForwardNetwork(nn.Module):
+    r"""Feed-forward network (FFN) in Transformer.
+
+    Args:
+        features (int): Dimensionality of the feature space.
+        ffn_ratio (int, optional): Expansion ratio of the hidden layer.
+            Default is `4`.
+        dropout_rate (float, optional): Dropout rate. Default is `0.0`.
+        activation (Callable, optional): Activation function.
+            Default is `approx_gelu_tanh`.
+        deterministic (bool, optional): Whether to apply dropout.
+            Default is `None`.
+        dtype (Any, optional): The data type of the computation.
+        param_dtype (Any, optional): The data type of the parameters.
+    """
+
+    features: int
+    ffn_ratio: int = 4
+    dropout_rate: float = 0.0
+    activation: typing.Callable[..., jax.Array] = approx_gelu_tanh
+    deterministic: typing.Optional[bool] = None
+    dtype: typing.Any = None
+    param_dtype: typing.Any = None
+
+    @nn.compact
+    def __call__(self, inputs: jax.Array) -> jax.Array:
+        r"""Forward pass of the feed-forward network.
+
+        Args:
+            inputs (jax.Array): Input tensor of shape `(*, L, C)`.
+
+        Returns:
+            Output tensor of shape `(*, L, C)`.
+        """
+        hidden_dim = self.features * self.ffn_ratio
+        out = nn.Dense(
+            features=hidden_dim,
+            dtype=self.dtype,
+            param_dtype=self.param_dtype,
+            name="fc_in",
+        )(inputs.astype(self.dtype))
+        out = self.activation(out)
+        if self.dropout_rate > 0.0:
+            m_deterministic = nn.merge_param(
+                "deterministic",
+                self.deterministic,
+                None,
+            )
+            out = nn.Dropout(
+                rate=self.dropout_rate,
+                deterministic=m_deterministic,
+                name="dropout",
+            )(out)
+        out = nn.Dense(
+            features=self.features,
+            dtype=self.dtype,
+            param_dtype=self.param_dtype,
+            name="fc_out",
+        )(out)
+        return out
+
+
+class _DiTBlock(nn.Module):
+    r"""General Diffusion Transformers (DiT) block.
+
+    Args:
+        features (int): Dimensionality of the feature space.
+        num_heads (int, optional): Number of attention heads. Default is `8`.
+        ffn_ratio (int, optional): Expansion ratio of the hidden layer.
+            Default is `4`.
+        deterministic (bool, optional): Whether to apply dropout.
+            Default is `None`.
+        dtype (Any, optional): The data type of the computation.
+        param_dtype (Any, optional): The data type of the parameters.
+        precision (Any, optional): Numerical precision of the computation.
+    """
+
+    features: int
+    num_heads: int = 8
+    ffn_ratio: int = 4
+    deterministic: typing.Optional[bool] = None
+    dtype: typing.Any = None
+    param_dtype: typing.Any = None
+    precision: typing.Any = None
+
+    @property
+    def block_type(self) -> str:
+        r"""str: One of 'in-context', 'cross-attention', or 'adaLN'."""
+        ...
+
+    def setup(self) -> None:
+        """Sets up the DiT block components."""
+        self.norm_1 = nn.LayerNorm(
+            epsilon=1e-6,
+            use_bias=not (self.block_type == "adaLN"),
+            use_scale=not (self.block_type == "adaLN"),
+            dtype=self.dtype,
+            param_dtype=self.param_dtype,
+            name="attn_norm",
+        )
+        self.attn = SelfAttention(
+            features=self.features,
+            num_heads=self.num_heads,
+            dtype=self.dtype,
+            param_dtype=self.param_dtype,
+            precision=self.precision,
+            name="attn",
+        )
+        self.norm_2 = nn.LayerNorm(
+            epsilon=1e-6,
+            use_bias=not (self.block_type == "adaLN"),
+            use_scale=not (self.block_type == "adaLN"),
+            dtype=self.dtype,
+            param_dtype=self.param_dtype,
+            name="ffn_norm",
+        )
+        self.mlp = FeedForwardNetwork(
+            features=self.features,
+            ffn_ratio=self.ffn_ratio,
+            dtype=self.dtype,
+            param_dtype=self.param_dtype,
+            name="ffn",
+        )
+        if self.block_type == "adaLN":
+            self.adaln_modulation = nn.Dense(
+                features=6 * self.features,
+                use_bias=True,
+                dtype=self.dtype,
+                param_dtype=self.param_dtype,
+                name="adaln_modulation",
+            )
+
+    def __call__(
+        self,
+        inputs: jax.Array,
+        cond: jax.Array,
+        deterministic: typing.Optional[bool] = None,
+    ) -> jax.Array:
+        r"""Forward pass of the DiT block.
+
+        Args:
+            inputs (jax.Array): Input tensor of shape `(*, L, C)`.
+            cond (jax.Array): Conditioning tensor of shape `(*, C)`.
+            deterministic (bool, optional): Whether to apply dropout.
+                Default is `None`.
+
+        Returns:
+            Output tensor of shape `(*, L, C)`.
+        """
+        m_deterministic = nn.merge_param(
+            "deterministic",
+            self.deterministic,
+            deterministic,
+        )
+        out = inputs.astype(self.dtype)
+        cond = cond.astype(self.dtype)
+        if self.block_type == "in-context":
+            out = jnp.concatenate([out, cond[:, None, :]], axis=-2)
+            skip = out
+            out = self.norm_1(out)
+            out = self.attn(out, deterministic=m_deterministic)
+            out = out + skip
+
+            skip = out
+            out = self.norm_2(out)
+            out = self.mlp(out)
+            out = out + skip
+        elif self.block_type == "cross-attention":
+            raise NotImplementedError
+        elif self.block_type == "adaLN":
+            (
+                shift_msa,
+                scale_msa,
+                gate_msa,
+                shift_ffn,
+                scale_ffn,
+                gate_msa,
+            ) = jnp.split(
+                self.adaln_modulation(jax.nn.silu(cond)),
+                indices_or_sections=6,
+                axis=-1,
+            )
+            out = out + gate_msa[..., None, :] * self.attn(
+                inputs=modulate(
+                    self.norm_1(out),
+                    shift=shift_msa,
+                    scale=scale_msa,
+                ),
+                deterministic=m_deterministic,
+            )
+            out = out + gate_msa[..., None, :] * self.mlp(
+                modulate(
+                    self.norm_2(out),
+                    shift=shift_ffn,
+                    scale=scale_ffn,
+                )
+            )
+
+        return out
+
+
+class DiTConditioningBlock(_DiTBlock):
     r"""Diffusion Transformers (DiT) block with in-context conditioning.
 
     Args:
+        features (int): Dimensionality of the feature space.
+        num_heads (int, optional): Number of attention heads. Default is `8`.
+        ffn_ratio (int, optional): Expansion ratio of the hidden layer.
+            Default is `4`.
+        deterministic (bool, optional): Whether to apply dropout.
+            Default is `None`.
+        dtype (Any, optional): The data type of the computation.
+        param_dtype (Any, optional): The data type of the parameters.
+        precision (Any, optional): Numerical precision of the computation.
     """
 
-    @nn.compact
-    def __call__(self, inputs: jax.Array, cond: jax.Array) -> jax.Array:
-        raise NotImplementedError
+    @property
+    def block_type(self) -> str:
+        return "in-context"
 
 
-class _DiTCrossAttentionBlock(nn.Module):
-    r"""Diffusion Transformers (DiT) block with cross-attention."""
+class DiTCrossAttentionBlock(_DiTBlock):
+    r"""Diffusion Transformers (DiT) block with cross-attention.
 
-    @nn.compact
-    def __call__(self, inputs: jax.Array, cond: jax.Array) -> jax.Array:
-        raise NotImplementedError
+    Args:
+        features (int): Dimensionality of the feature space.
+        num_heads (int, optional): Number of attention heads. Default is `8`.
+        ffn_ratio (int, optional): Expansion ratio of the hidden layer.
+            Default is `4`.
+        deterministic (bool, optional): Whether to apply dropout.
+            Default is `None`.
+        dtype (Any, optional): The data type of the computation.
+        param_dtype (Any, optional): The data type of the parameters.
+        precision (Any, optional): Numerical precision of the computation.
+    """
+
+    @property
+    def block_type(self) -> str:
+        return "cross-attention"
 
 
-class _DiTAdaLNBlock(nn.Module):
-    r"""Diffusion Transformers (DiT) block with adaptive layer normalization."""
+class DiTAdaLNBlock(_DiTBlock):
+    r"""Diffusion Transformers (DiT) block with adaptive layer normalization.
 
-    @nn.compact
-    def __call__(self, inputs: jax.Array, cond: jax.Array) -> jax.Array:
-        raise NotImplementedError
+    Args:
+        features (int): Dimensionality of the feature space.
+        num_heads (int, optional): Number of attention heads. Default is `8`.
+        ffn_ratio (int, optional): Expansion ratio of the hidden layer.
+            Default is `4`.
+        deterministic (bool, optional): Whether to apply dropout.
+            Default is `None`.
+        dtype (Any, optional): The data type of the computation.
+        param_dtype (Any, optional): The data type of the parameters.
+        precision (Any, optional): Numerical precision of the computation.
+    """
+
+    @property
+    def block_type(self) -> str:
+        return "adaLN"
--- src/projects/generative/model/test_dit.py
+++ src/projects/generative/model/test_dit.py
@@ -35,5 +35,96 @@ def test_self_attention(num_heads: int, dtype: typing.Any) -> None:
     assert test_output.dtype == dtype
 
 
+@pytest.mark.parametrize("dtype", [jnp.bfloat16, jnp.float32])
+def test_in_context_dit_block(dtype: typing.Any) -> None:
+    r"""Test the DiT block with in-context conditioning."""
+    layer = dit.DiTConditioningBlock(
+        features=16,
+        num_heads=4,
+        ffn_ratio=4,
+        dtype=dtype,
+        param_dtype=dtype,
+    )
+    test_input = jnp.ones((2, 8, 16), dtype=dtype)
+    test_cond = jnp.ones((2, 16), dtype=dtype)
+    variables = layer.init(
+        jax.random.PRNGKey(0),
+        test_input,
+        cond=test_cond,
+        deterministic=True,
+    )
+    test_output = layer.apply(
+        variables,
+        test_input,
+        cond=test_cond,
+        deterministic=False,
+        rngs={"dropout": jax.random.PRNGKey(1)},
+    )
+    assert isinstance(test_output, jax.Array)
+    assert test_output.shape == (2, 9, 16)
+    assert test_output.dtype == dtype
+
+
+@pytest.mark.skip("Not yet implemented")
+@pytest.mark.parametrize("dtype", [jnp.bfloat16, jnp.float32])
+def test_cross_attention_dit_block(dtype: typing.Any) -> None:
+    r"""Test the DiT block with cross-attention."""
+    layer = dit.DiTCrossAttentionBlock(
+        features=16,
+        num_heads=4,
+        ffn_ratio=4,
+        dtype=dtype,
+        param_dtype=dtype,
+    )
+    test_input = jnp.ones((2, 8, 16), dtype=dtype)
+    test_context = jnp.ones((2, 6, 16), dtype=dtype)
+    variables = layer.init(
+        jax.random.PRNGKey(0),
+        test_input,
+        context=test_context,
+        deterministic=True,
+    )
+    test_output = layer.apply(
+        variables,
+        test_input,
+        context=test_context,
+        deterministic=False,
+        rngs={"dropout": jax.random.PRNGKey(1)},
+    )
+    assert isinstance(test_output, jax.Array)
+    assert test_output.shape == (2, 8, 16)
+    assert test_output.dtype == dtype
+
+
+@pytest.mark.parametrize("dtype", [jnp.bfloat16, jnp.float32])
+def test_adaln_dit_block(dtype: typing.Any) -> None:
+    r"""Test the DiT block with adaptive layer normalization."""
+    layer = dit.DiTAdaLNBlock(
+        features=16,
+        num_heads=4,
+        ffn_ratio=4,
+        dtype=dtype,
+        param_dtype=dtype,
+    )
+    test_input = jnp.ones((2, 8, 16), dtype=dtype)
+    test_cond = jnp.ones((2, 16), dtype=dtype)
+    variables = layer.init(
+        jax.random.PRNGKey(0),
+        test_input,
+        cond=test_cond,
+        deterministic=True,
+    )
+    test_output = layer.apply(
+        variables,
+        test_input,
+        cond=test_cond,
+        deterministic=False,
+        rngs={"dropout": jax.random.PRNGKey(1)},
+    )
+    assert isinstance(test_output, jax.Array)
+    assert test_output.shape == (2, 8, 16)
+    assert test_output.dtype == dtype
+
+
 if __name__ == "__main__":
     sys.exit(pytest.main(["-xv", __file__]))
