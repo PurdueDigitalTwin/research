2ccd847611667b9021bfdf3a1b16a17ba8850e5a
Juanwu <juanwu@purdue.edu>
2026-01-16 17:37:01 -0500

    feat: Initial implementation of the DDPM wrapper
    Signed-off-by: Juanwu <juanwu@purdue.edu>

--- src/projects/generative/ddpm.py
+++ src/projects/generative/ddpm.py
@@ -5,7 +5,9 @@ from flax import linen as nn
 import jax
 from jax import numpy as jnp
 import jaxtyping
+import typing_extensions
 
+from src.core import model as _model
 from src.projects.generative.model import unet
 
 # Type aliases
@@ -153,3 +155,112 @@ class DDPMUNetModule(nn.Module):
         output = backbone(inputs, cond=t_emb, deterministic=m_deterministic)
 
         return output
+
+
+class DDPMGaussianUNetModel(_model.Model):
+    r"""Denoising Deep Probabilistic Models with U-Net and Gaussian noises.
+
+    Args:
+        in_channels (int): Number of input channels.
+        image_size (int): Height and width of the (squared) input images.
+        features (int): Base number of features for the latent representations.
+        dropout_rate (float): Dropout rate for regularization.
+        epsilon (float): Small constant for numerical stability in
+            the group normalization layers.
+        attn_resolutions (typing.Sequence[int]): Resolutions at which to apply
+            attention following the residual convolution block.
+        num_res_blocks (int): Number of residual blocks per level of the U-Net.
+        resample_with_conv (bool): Whether to use convolutional layers for
+            upsampling and downsampling operations. Default is `True`.
+        predict_variance (bool): Whether the model predicts variance along with
+            the mean. If `True`, the output channels will be
+            `in_channels * 2`. Default is `False`.
+        dtype (typing.Any, optional): Data type for computations.
+        param_dtype (typing.Any, optional): Data type for parameters.
+        precision (typing.Any, optional): Numerical precision for computations.
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        image_size: int,
+        features: int,
+        dropout_rate: float,
+        epsilon: float,
+        attn_resolutions: typing.Sequence[int],
+        num_res_blocks: int,
+        resample_with_conv: bool = True,
+        predict_variance: bool = False,
+        dtype: typing.Any = None,
+        param_dtype: typing.Any = None,
+        precision: typing.Any = None,
+    ) -> None:
+        """Initialize the DDPM U-Net model."""
+        self.in_channels = in_channels
+        self.image_size = image_size
+        self.features = features
+        self.dtype = dtype
+        self.param_dtype = param_dtype
+
+        self._network = DDPMUNetModule(
+            features=features,
+            out_features=in_channels * 2 if predict_variance else in_channels,
+            resample_with_conv=resample_with_conv,
+            attn_resolutions=attn_resolutions,
+            num_res_blocks=num_res_blocks,
+            dropout_rate=dropout_rate,
+            epsilon=epsilon,
+            name="unet",
+            dtype=dtype,
+            param_dtype=param_dtype,
+            precision=precision,
+        )
+
+    @property
+    @typing_extensions.override
+    def network(self) -> DDPMUNetModule:
+        r"""DDPMUNetModule: The underlying neural network module."""
+        return self._network
+
+    def init(
+        self,
+        *,
+        batch: typing.Any,
+        rngs: typing.Any,
+        **kwargs,
+    ) -> PyTree:
+        del batch, kwargs  # unused
+
+        variables = self.network.init(
+            rngs,
+            inputs=jnp.zeros(
+                (1, self.image_size, self.image_size, self.in_channels),
+                dtype=self.dtype,
+            ),
+            timestep=jnp.zeros((1,), dtype=self.dtype),
+            deterministic=True,
+        )
+        tabulate_fn = nn.summary.tabulate(
+            self.network,
+            depth=3,
+            rngs=rngs,
+            console_kwargs={"width": 120},
+        )
+        if tabulate_fn is not None:
+            print(
+                tabulate_fn(
+                    inputs=jnp.zeros(
+                        (
+                            1,
+                            self.image_size,
+                            self.image_size,
+                            self.in_channels,
+                        ),
+                        dtype=self.dtype,
+                    ),
+                    timestep=jnp.zeros((1,), dtype=self.dtype),
+                    deterministic=True,
+                )
+            )
+
+        return variables["params"]
