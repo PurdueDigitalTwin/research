fdf3916006c002c57e157120f6132cbab71f06b7
Juanwu Lu <juanwu@purdue.edu>
2026-02-23 03:50:56 -0500

    feat: Implements the conditional flow-matching loss for UA-Flow pretraining
    Signed-off-by: Juanwu Lu <juanwu@purdue.edu>

--- src/projects/generative/experimental/run_ua_flow.py
+++ src/projects/generative/experimental/run_ua_flow.py
@@ -5,6 +5,7 @@ from flax import linen as nn
 import jax
 from jax import numpy as jnp
 import jaxtyping
+import optax
 
 from src.core import model as _model
 from src.core import train_state as _train_state
@@ -224,7 +225,6 @@ class UAFlowUNetModel:
         # construct the ADM U-Net architecture
         self._network = UAFlowUNetModule(
             features=features,
-            out_features=in_channels * 2,
             ch_mults=ch_mults,
             attn_resolutions=attn_resolutions,
             num_res_blocks=num_res_blocks,
@@ -249,9 +249,105 @@ class UAFlowUNetModel:
         batch: jaxtyping.PyTree,
         deterministic: bool,
         rngs: jax.Array,
+        with_variance: bool = False,
         **kwargs,
     ) -> typing.Tuple[_train_state.TrainState, _model.StepOutputs]:
-        raise NotImplementedError
+        r"""Computes and returns the training loss in a single gradient step.
+
+        Args:
+            state (TrainState): The current training state containing model
+                parameters and optimizer state.
+            batch (PyTree): A batch of training data.
+            deterministic (bool): Whether to run the model in deterministic mode
+                (e.g., disable dropout).
+            rngs (jax.Array): Random generators for stochastic operations.
+            with_variance (bool, optional): Whether to predict the variance and
+                apply the `CUFM` loss. Default is `False`.
+
+        Returns:
+            A tuple of the updated training state and a dictionary
+                containing the computed loss and other outputs.
+        """
+
+        local_rng = jax.random.fold_in(rngs, jax.lax.axis_index("batch"))
+        local_rng = jax.random.fold_in(local_rng, state.step)
+        flip_key, t_key, e_key, dropout_key = jax.random.split(local_rng, 4)
+
+        # pre-processing
+        image = batch.get("image")
+        if not isinstance(image, jax.Array):
+            raise ValueError("Invalid `image` in batch for training step.")
+        *batch_dims, height, width, channels = image.shape
+        image = image.astype(self._dtype).reshape(-1, height, width, channels)
+        image = image * 2.0 - 1.0  # NOTE: scale to [-1, 1]
+        mask = jax.random.uniform(key=flip_key, shape=image.shape[0:1]) < 0.5
+        image = jnp.where(
+            mask[:, None, None, None],
+            jnp.flip(image, axis=-2),
+            image,
+        )
+
+        # sample random time steps
+        # TODO (juanwu): consider sampling from logit-normal
+        t = jax.random.randint(
+            key=t_key,
+            shape=batch_dims,
+            minval=0.0,
+            maxval=1.0,
+            dtype=self._dtype,
+        ).reshape(-1, 1)
+        t_expanded = t[..., None, None, :]
+
+        # construct the cfm target
+        e = jax.random.normal(key=e_key, shape=image.shape, dtype=self._dtype)
+        x_t = (1 - t_expanded) * image + t_expanded * e
+        v_target = jax.lax.stop_gradient(e - image)
+
+        def cfm_loss(params: jaxtyping.PyTree) -> jax.Array:
+            r"""Computes the conditional flow-matching loss.
+
+            Args:
+                params (jaxtyping.PyTree): A tree of model parameters.
+
+            Returns:
+                A scalar array the computed loss value.
+            """
+            v_pred, _ = self._network.apply(
+                variables={"params": params},
+                inputs=x_t,
+                timestep=t,
+                deterministic=deterministic,
+                rngs={"dropout": dropout_key},
+            )
+            if not isinstance(v_pred, jax.Array):
+                raise TypeError(
+                    f"Output has a type of {type(v_pred)}, "
+                    "but expect a ``jax.Array`` instead."
+                )
+
+            loss = optax.squared_error(v_pred, v_target).mean()
+            return loss
+
+        def cufm_loss(
+            params: jaxtyping.PyTree,
+        ) -> typing.Tuple[jax.Array, _model.StepOutputs]:
+            raise NotImplementedError
+
+        if with_variance:
+            grad_fn = jax.value_and_grad(cufm_loss, has_aux=False)
+            loss, grads = grad_fn(state.params)
+        else:
+            grad_fn = jax.value_and_grad(cfm_loss, has_aux=False)
+            loss, grads = grad_fn(state.params)
+        loss = jax.lax.pmean(loss, axis_name="batch")
+        grads = jax.lax.pmean(grads, axis_name="batch")
+        new_state = state.apply_gradients(grads=grads)
+
+        output = _model.StepOutputs(
+            scalars={"loss": loss}, histograms={"timestep": t}
+        )
+
+        return new_state, output
 
     def evaluation_step(
         self,
