b7fe1a775354c230d47634779148db68ce433d0a
Juanwu Lu <juanwu@purdue.edu>
2026-02-27 03:46:13 -0500

    feat: Implemented the training step for Actor-Critic model
    Signed-off-by: Juanwu Lu <juanwu@purdue.edu>

--- src/projects/rl/experimental/run_ac.py
+++ src/projects/rl/experimental/run_ac.py
@@ -5,7 +5,7 @@ from absl import app
 from absl import flags
 from flax import jax_utils
 from flax import linen as nn
-from flax import serialization
+from flax.training import common_utils
 import gymnasium as gym
 import jax
 from jax import numpy as jnp
@@ -15,6 +15,7 @@ import optax
 from src.core import model as _model
 from src.core import train_state as _train_state
 from src.projects.rl import replay_buffer as _replay_buffer
+from src.projects.rl import structure as _structure
 from src.utilities import logging as _logging
 
 # Flags
@@ -32,6 +33,11 @@ flags.DEFINE_float(
     name="gamma",
     default=0.99,
     help="Discount factor for future rewards in the reinforcement learning.",
+)
+flags.DEFINE_integer(
+    name="num_episodes",
+    default=50,
+    help="Number of episodes to train the Actor-Critic model.",
 )
 flags.DEFINE_integer(
     name="seed",
@@ -162,17 +168,101 @@ class ActorCriticModel:
     def training_step(
         self,
         *,
-        batch: jaxtyping.PyTree,
+        batch: _structure.StepTuple,
         state: _train_state.TrainState,
         rngs: typing.Any,
         **kwargs,
-    ) -> _model.StepOutputs:
+    ) -> typing.Tuple[_train_state.TrainState, _model.StepOutputs]:
         del kwargs
 
         local_rng = jax.random.fold_in(rngs, jax.lax.axis_index("batch"))
         local_rng = jax.random.fold_in(local_rng, state.step)
 
-        raise NotImplementedError
+        # step 1: compute TD-error and update the Q-function
+        def critic_loss_fn(params: jaxtyping.PyTree) -> jax.Array:
+            if batch.action is None:
+                raise ValueError("Action is required for Q-learning.")
+            outputs = self.network.apply(
+                variables={"params": params},
+                state=batch.state,
+                rngs=local_rng,
+            )
+            assert isinstance(outputs, typing.Dict)
+            q_vals = outputs["critic"]["q_values"]
+            one_hot_a = jax.nn.one_hot(
+                batch.action[..., 0].astype(jnp.int32),
+                num_classes=q_vals.shape[-1],
+            )
+            q_vals = jnp.sum(q_vals * jax.lax.stop_gradient(one_hot_a), -1)
+
+            outputs = self.network.apply(
+                variables={"params": params},
+                state=batch.next_state,
+                rngs=local_rng,
+            )
+            assert isinstance(outputs, typing.Dict)
+            prob_actions = jax.nn.softmax(outputs["actor"]["logits"])
+            q_next = jnp.sum(
+                prob_actions * outputs["critic"]["q_values"],
+                axis=-1,
+            )
+
+            if batch.reward is None:
+                r = jnp.zeros_like(q_next)
+            else:
+                r = batch.reward.astype(q_next)
+
+            if batch.done is None:
+                d = jnp.zeros_like(q_next)
+            else:
+                d = batch.done.astype(q_next.dtype)
+
+            q_tgt = jax.lax.stop_gradient(r + self._gamma * q_next * (1 - d))
+            loss = optax.squared_error(q_vals, q_tgt).mean()
+
+            return loss
+
+        grad_fn = jax.value_and_grad(critic_loss_fn, has_aux=False)
+        critic_loss, critic_grads = grad_fn(state.params)
+        critic_loss = jax.lax.pmean(critic_loss, axis_name="batch")
+        critic_grads = jax.lax.pmean(critic_grads, axis_name="batch")
+        new_state = state.apply_gradients(grads=critic_grads)
+        new_state = new_state.replace(step=state.step)
+
+        # step 2: compute the policy loss and update the policy function
+        def actor_loss_fn(params: jaxtyping.PyTree) -> jax.Array:
+            outputs = self.network.apply(
+                variables={"params": params},
+                state=batch.state,
+                rngs=local_rng,
+            )
+            assert isinstance(outputs, typing.Dict)
+            q_vals = jax.lax.stop_gradient(outputs["critic"]["q_values"])
+            logits = outputs["actor"]["logits"]
+
+            loss = jnp.sum(
+                -jax.nn.softmax(logits, axis=-1) * q_vals,
+                axis=-1,
+            ).mean()
+
+            return loss
+
+        grad_fn = jax.value_and_grad(actor_loss_fn, has_aux=False)
+        actor_loss, actor_grads = grad_fn(new_state.params)
+        actor_loss = jax.lax.pmean(actor_loss, axis_name="batch")
+        actor_grads = jax.lax.pmean(actor_grads, axis_name="batch")
+        new_state = new_state.apply_gradients(grads=actor_grads)
+
+        outputs = _model.StepOutputs(
+            scalars=dict(
+                actor_loss=actor_loss.mean(),
+                actor_grad_norm=optax.global_norm(actor_grads).mean(),
+                critic_loss=critic_loss.mean(),
+                critic_grad_norm=optax.global_norm(critic_grads).mean(),
+            )
+        )
+
+        return new_state, outputs
 
     def evaluation_step(
         self,
@@ -265,19 +355,13 @@ def main(argv: typing.List[str]) -> None:
     p_predict_step = functools.partial(model.predict_step, rngs=predict_key)
     p_predict_step = jax.pmap(p_predict_step, axis_name="batch")
 
+    train_step_cntr: int = int(train_state.step)
     train_state: _train_state.TrainState = jax_utils.replicate(train_state)
     try:
         _logging.rank_zero_info("Populating the replay buffer...")
         state, _ = env.reset()
-        for step in range(flags.FLAGS.buffer_capacity):
-            batch = jax.tree_util.tree_map(
-                lambda x: (
-                    x.reshape(jax.local_device_count(), -1)
-                    if hasattr(x, "reshape")
-                    else x
-                ),
-                state,
-            )
+        for _ in range(flags.FLAGS.buffer_capacity):
+            batch = jax.tree_util.tree_map(common_utils.shard, state)
             action = p_predict_step(batch=batch, params=train_state.params)
             action = jax.device_get(action).reshape(-1).item()
             next_state, reward, terminated, truncated, _ = env.step(action)
@@ -291,6 +375,27 @@ def main(argv: typing.List[str]) -> None:
 
         # main training loop
         _logging.rank_zero_info("Training...")
+        rngs, sample_key = jax.random.split(rngs, num=2)
+        for episode in range(flags.FLAGS.num_episodes + 1):
+            state, _ = env.reset()
+            done = False
+
+            while not done:
+                # sample a batch of transition tuples
+                sample_key = jax.random.fold_in(sample_key, train_step_cntr)
+                batch = buffer.sample(flags.FLAGS.batch_size, sample_key)
+                assert isinstance(batch, _structure.StepTuple)
+                leaves, tree_def = jax.tree_util.tree_flatten(batch)
+                leaves = jax.tree_util.tree_map(common_utils.shard, leaves)
+                batch = jax.tree_util.tree_unflatten(tree_def, leaves)
+                train_state, outputs = p_train_step(
+                    batch=batch,
+                    state=train_state,
+                )
+                print(outputs)
+
+                train_step_cntr += 1
+
     finally:
         train_state = jax_utils.unreplicate(train_state)
 
