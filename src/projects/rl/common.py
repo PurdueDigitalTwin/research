2198937689c9462919d5cb590aa435212f8261f3
Juanwu Lu <juanwu@purdue.edu>
2026-03-08 19:24:44 -0400

    feat: Implemented the base agent and environment classes
    Signed-off-by: Juanwu Lu <juanwu@purdue.edu>

--- src/projects/rl/common.py
+++ src/projects/rl/common.py
@@ -1,7 +1,47 @@
+import abc
 import typing
 
 import chex
 import jax
+
+from src.core import model as _model
+
+
+class BaseAgent(_model.Model, abc.ABC):
+    r"""Base class for reinforcement learning agents."""
+
+    @property
+    @abc.abstractmethod
+    def is_on_policy(self) -> bool:
+        r"""bool: Whether the agent runs an on-policy RL algorithm."""
+        ...
+
+    def on_train_batch_end(self, state: typing.Any, **kwargs) -> typing.Any:
+        r"""Called at the end of a single training step."""
+        return state  # NOTE: no-op by default.
+
+
+class BaseEnvironment(abc.ABC):
+    r"""Base class for reinforcement learning environment."""
+
+    @abc.abstractmethod
+    def step(self, action: typing.Any) -> typing.Any:
+        r"""Take action and run one time step in the environment."""
+        ...
+
+    @abc.abstractmethod
+    def reset(
+        self,
+        *,
+        seed: typing.Optional[int] = None,
+        **kwargs,
+    ) -> typing.Any:
+        r"""Resets the environment and returns an initial state."""
+        ...
+
+    def close(self) -> None:
+        r"""Destructor for the environment."""
+        pass  # NOTE: no-op by default
 
 
 @chex.dataclass
@@ -26,3 +66,6 @@ class StepTuple:
     reward: typing.Optional[jax.Array] = None
     next_state: typing.Optional[jax.Array] = None
     done: typing.Optional[jax.Array] = None
+
+
+__all__ = ["BaseAgent", "StepTuple"]
