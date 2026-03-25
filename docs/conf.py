dc3b5c139c5f9c2077712dff72a210872e72ff48
Juanwu Lu <juanwu@purdue.edu>
2026-03-25 12:36:45 -0400

    feat: Implemented the CSS style for the documentation page
    Signed-off-by: Juanwu Lu <juanwu@purdue.edu>

--- docs/conf.py
+++ docs/conf.py
@@ -22,5 +22,21 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
+
+html_logo = "_static/ENG_PDT_Full-RGB-Light.png"
+html_favicon = "_static/ENG_PDT_Full-RGB-Light.png"
+
+html_theme_options = {
+    "logo": {
+        "image_light": "_static/ENG_PDT_Full-RGB-Light.png",
+    },
+    "repository_url": "https://github.com/PurdueDigitalTwin/research",
+    "use_repository_button": True,
+    "show_toc_level": 2,
+    "navigation_with_keys": False,
+}
+
+html_title = "PDT Research"
