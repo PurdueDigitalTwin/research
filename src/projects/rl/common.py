08a96cae624eca8a7fdf4f3ec1a2785df80a48a4
Juanwu Lu <juanwu@purdue.edu>
2026-03-26 01:10:39 -0400

    feat: Updated common base classes for RL
    Signed-off-by: Juanwu Lu <juanwu@purdue.edu>

--- src/projects/rl/common.py
+++ src/projects/rl/common.py
@@ -24,6 +24,29 @@ class BaseAgent(_model.Model, abc.ABC):
         r"""bool: Whether the agent runs an on-policy RL algorithm."""
         ...
 
+    @abc.abstractmethod
+    def get_action(
+        self,
+        *,
+        obs: jax.Array,
+        params: typing.Any,
+        rngs: typing.Any,
+        deterministic: bool = False,
+        **kwargs,
+    ) -> typing.Union[jax.Array, typing.Tuple[typing.Any, ...]]:
+        r"""Execute the policy and returns the action.
+
+        Args:
+            obs (jax.Array): State observation array.
+            params (typing.Any): Parameters for the policy.
+            rngs (typing.Any): Random generator for reproducibility.
+            deterministic (bool): Whether to run policy in deterministic mode.
+
+        Returns:
+            A single action array or a tuple of action and related outputs.
+        """
+        ...
+
     def on_train_batch_end(self, *, state: typing.Any, **kwargs) -> typing.Any:
         r"""Called at the end of a single training step."""
         return state  # NOTE: no-op by default.
@@ -54,6 +77,26 @@ class BaseEnvironment(abc.ABC):
 
 @dataclasses.dataclass(frozen=False, kw_only=True)
 class RLExperimentConfig:
+    r"""Experiment configurations for running reinforcement learning algorithms.
+
+    Attributes:
+        project_name (str): Name of the project.
+        exp_name (str): Name of the experiment.
+        agent (fiddle.Partial): Partial function to instantiate the RL agent.
+        environment (fiddle.Config): Build target for RL environment.
+        trainer (TrainerConfig): Configurations for training.
+        optimizer (OptimizerConfig): Configurations for the optimizers.
+        replay_buffer_capacity (int): Number of samples in the replay buffer.
+        warmup_steps (int): Random actions before training starts.
+        n_envs (int): Number of parallel environments to run for on-policy RL.
+        n_rollout_steps (int): Number of rollout steps per environment.
+        gae_lambda (float): Discount factor in generalized advantage estimates.
+        dtype (Any): The data type of the computation.
+        param_dtype (Any): The data type of the parameters.
+        precision (Any): Numerical precision of the computation.
+        seed (int): Seed of random generator for reproducibility.
+    """
+
     project_name: str
     exp_name: str
 
@@ -62,6 +105,15 @@ class RLExperimentConfig:
     environment: fdl.Config[BaseEnvironment]
     trainer: _config.TrainerConfig
     optimizer: _config.OptimizerConfig
+
+    # replay buffer configurations
+    replay_buffer_capacity: int = 1_000_000
+    warmup_steps: int = 1_000
+
+    # rollout configurations for on-policy RL
+    n_envs: int = 1
+    n_rollout_steps: int = 2_048
+    gae_lambda: float = 0.95
 
     # global settings
     dtype: typing.Any = None
@@ -94,4 +146,4 @@ class StepTuple:
     done: typing.Optional[jax.Array] = None
 
 
-__all__ = ["BaseAgent", "StepTuple"]
+__all__ = ["BaseAgent", "BaseEnvironment", "RLExperimentConfig", "StepTuple"]
