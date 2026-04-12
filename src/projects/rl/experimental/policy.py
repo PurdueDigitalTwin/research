5d85d2f981296bad6e3e5ca5e04eea3052e30a22
Juanwu <juanwu@purdue.edu>
2026-04-12 19:01:02 -0400

    feat: Implemented the state-independent standard deviation in Gaussian policy
    Signed-off-by: Juanwu <juanwu@purdue.edu>

--- src/projects/rl/experimental/policy.py
+++ src/projects/rl/experimental/policy.py
@@ -78,18 +78,30 @@ class GaussianPolicy(nn.Module):
 
     Attributes:
         features (int): Dimensionality of the hidden features.
+        out_features (int): Dimensionality of the action space.
+        num_layers (int): Number of layers in the MLP.
+        activation (Callable[[jax.Array], jax.Array]): Activation function
+            to use between layers (e.g., ``jax.nn.relu``).
+        use_state_dependent_std (bool, optional): Whether the standard deviation
+            is state-dependent. Default is ``False``.
+        dtype (Any, optional): Data type for the computations.
+            Default is ``None``.
+        param_dtype (Any, optional): Data type for the parameters.
+            Default is ``None``.
     """
 
     features: int
     out_features: int
     num_layers: int
     activation: typing.Callable[[jax.Array], jax.Array]
+    use_state_dependent_std: bool = False
     dtype: typing.Any = None
     param_dtype: typing.Any = None
 
     @nn.compact
     def __call__(
-        self, inputs: jax.Array
+        self,
+        inputs: jax.Array,
     ) -> typing.Tuple[jax.Array, jax.Array]:
         r"""Forward pass the Gaussian policy network `\pi(a|s;\theta)`
 
@@ -126,21 +138,42 @@ class GaussianPolicy(nn.Module):
             out = fc(out)
             out = self.activation(out)
 
-        fc_out = nn.Dense(
-            features=self.out_features * 2,  # output mean and log_std together
-            kernel_init=kernel_init,
-            use_bias=True,  # use bias term
-            bias_init=jax.nn.initializers.zeros,
-            dtype=self.dtype,
-            param_dtype=self.param_dtype,
-            name="fc_out",
-        )
-        out = fc_out(out)
-        mean, log_std = jnp.split(
-            out, 2, axis=-1
-        )  # split into mean and log_std
-        log_std = jnp.clip(
-            log_std, -5.0, 2.0
-        )  # clip log_std for numerical stability
+        if self.use_state_dependent_std:
+            fc_out = nn.Dense(
+                features=self.out_features
+                * 2,  # output mean and log_std together
+                kernel_init=kernel_init,
+                use_bias=True,  # use bias term
+                bias_init=jax.nn.initializers.zeros,
+                dtype=self.dtype,
+                param_dtype=self.param_dtype,
+                name="fc_out",
+            )
+            out = fc_out(out)
+            mean, log_std = jnp.split(
+                out, 2, axis=-1
+            )  # split into mean and log_std
+            log_std = jnp.clip(
+                log_std, -5.0, 2.0
+            )  # clip log_std for numerical stability
+        else:
+            fc_mean = nn.Dense(
+                features=self.out_features,
+                kernel_init=kernel_init,
+                use_bias=True,  # use bias term
+                bias_init=jax.nn.initializers.zeros,
+                dtype=self.dtype,
+                param_dtype=self.param_dtype,
+                name="fc_mean",
+            )
+            mean = fc_mean(out)
+
+            log_std_param = self.param(
+                "log_std",
+                jax.nn.initializers.zeros,
+                (self.out_features,),
+                self.param_dtype,
+            )
+            log_std = jnp.broadcast_to(log_std_param, mean.shape)
 
         return mean, log_std
